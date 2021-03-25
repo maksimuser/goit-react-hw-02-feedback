ffa7c7e56abba2e17ac65bb29c1275c42f0d9b10
Dema Maksim <maksimdema@gmail.com>
2021-03-25 13:00:59 +0200

    Updates

---
+++ static/js/main.6840c291.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),o=c(3),i=c.n(o),d=c(0),s=function(){return Object(d.jsx)("div",{children:"Hello world"})};i.a.render(Object(d.jsx)(r.a.StrictMode,{children:Object(d.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.6840c291.chunk.js.map
